b8d1ad041ad5608db439833c98f587f0dba15cd2
Mathsmaniac <smithn4@middleton.school.nz>
2022-03-17 14:42:09 +1300

    Create Movie.py

---
+++ Movie.py
@@ -0,0 +1,27 @@
+# Import statements
+
+# Functions
+
+# Main routine
+
+# Data structures
+
+# Ask for instructions
+
+# Loop to get ticket details:
+
+#   Get name (can't be blank)
+
+#   Get age (between 12 and 130)
+
+#   Calculate ticket price
+
+#   Ask for snacks
+
+#   Calculate snack price
+
+#   Ask for method of payment (apply surcharge if needed)
+
+# Calculate total sales and profit
+
+# Output data to file
